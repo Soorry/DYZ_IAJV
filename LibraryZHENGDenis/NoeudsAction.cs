a4c6bffc0745006a372c7b6b7810cb1640747b6e
Denis ZHENG <dzheng2@myges.fr>
2024-05-28 09:59:59 +0200

    Fix namespace

---
+++ LibraryZHENGDenis/NoeudsAction.cs
@@ -0,0 +1,23 @@
+﻿using AI_BehaviorTree_AIGameUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using LibraryCommon;
+
+namespace LibraryZHENGDenis
+{
+    class NoeudsFire : INoeud
+    {
+        public List<AIAction> actions { get => actions; set => actions = value; }
+
+        public etatNoeud Execute(object param)
+        {
+            actions.Add(new AIActionFire());
+            return etatNoeud.Sucess;
+        }
+    }
+}
