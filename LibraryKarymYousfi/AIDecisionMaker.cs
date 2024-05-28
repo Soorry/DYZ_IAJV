53dd9d2851d92a76f57ed8b8cb33ed8fb446da64
KrymoXCIII <karym.yousfi@gmail.com>
2024-05-28 11:08:39 +0200

    Readd LibraryKarymYousfi

---
+++ LibraryKarymYousfi/AIDecisionMaker.cs
@@ -0,0 +1,56 @@
+﻿using AI_BehaviorTree_AIGameUtility;
+using LibraryCommon;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace AI_BehaviorTree_AIImplementation
+{
+    public class AIDecisionMaker
+    {
+
+        /// <summary>
+        /// Ne pas supprimer des fonctions, ou changer leur signature sinon la DLL ne fonctionnera plus
+        /// Vous pouvez unitquement modifier l'intérieur des fonctions si nécessaire (par exemple le nom)
+        /// ComputeAIDecision en fait partit
+        /// </summary>
+        private int AIId = -1;
+        public GameWorldUtils AIGameWorldUtils = new GameWorldUtils();
+
+        // Ne pas utiliser cette fonction, elle n'est utile que pour le jeu qui vous Set votre Id, si vous voulez votre Id utilisez AIId
+        public void SetAIId(int parAIId) { AIId = parAIId; }
+
+        // Vous pouvez modifier le contenu de cette fonction pour modifier votre nom en jeu
+        public string GetName() { return "Narutode"; }
+
+        public void SetAIGameWorldUtils(GameWorldUtils parGameWorldUtils) { AIGameWorldUtils = parGameWorldUtils; }
+
+        //Fin du bloc de fonction nécessaire (Attention ComputeAIDecision en fait aussi partit)
+
+        public List<AIAction> ComputeAIDecision()
+        {
+            List<AIAction> actionList = new List<AIAction>();
+            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+
+            Selector start = new Selector();
+            start.noeuds.Add(new NoeudsFire());
+            start.Execute(null,actionList);
+            return actionList;
+        }
+
+        public PlayerInformations GetPlayerInfos(int parPlayerId, List<PlayerInformations> parPlayerInfosList)
+        {
+            foreach (PlayerInformations playerInfo in parPlayerInfosList)
+            {
+                if (playerInfo.PlayerId == parPlayerId)
+                    return playerInfo;
+            }
+
+            Assert.IsTrue(false, "GetPlayerInfos : PlayerId not Found");
+            return null;
+        }
+    }
+}
