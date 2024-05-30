3cd4f1d82a9458e1a60679c40191575b077256d9
Soorry <77273729+Soorry@users.noreply.github.com>
2024-05-30 13:59:06 +0200

    Ajout noeuds custom

--- LibraryRemiDugenet/AIDecisionMaker.cs
+++ LibraryRemiDugenet/AIDecisionMaker.cs
@@ -24,14 +24,29 @@ namespace AI_BehaviorTree_AIImplementation
         public AIDecisionMaker()
         {
             Selector start = new Selector();
-            Sequence seq = new Sequence();
-            MoveToPlayer move = new MoveToPlayer();
+            Sequence seq1 = new Sequence();
+            Sequence seq2 = new Sequence();
+            Sequence seq3 = new Sequence();
+
+            AvoidBullet avoid = new AvoidBullet();
+            seq1. noeuds.Add(avoid);
+            start.noeuds.Add(seq1);
+
+            MoveToBonus bonus = new MoveToBonus();
+            seq2.noeuds.Add(bonus);
+            start.noeuds.Add(seq2);
+
+            //MoveToPlayer move = new MoveToPlayer();
             LookAtPlayer lookAt = new LookAtPlayer();
+            CheckAngle angle = new CheckAngle();
+            SetTarget target = new SetTarget();
             Fire fire = new Fire();
-            seq.noeuds.Add(move);
-            seq.noeuds.Add(lookAt);
-            seq.noeuds.Add(fire);
-            start.noeuds.Add(seq);
+            seq3.noeuds.Add(target);
+            seq3.noeuds.Add(lookAt);
+            seq3.noeuds.Add(angle);
+            seq3.noeuds.Add(fire);
+            start.noeuds.Add(seq3);
+
             behaviourTree = new BehaviourTree(start, AIGameWorldUtils);
         }
 
@@ -39,7 +54,7 @@ namespace AI_BehaviorTree_AIImplementation
         public void SetAIId(int parAIId) { AIId = parAIId; }
 
         // Vous pouvez modifier le contenu de cette fonction pour modifier votre nom en jeu
-        public string GetName() { return "bot_bozo 2"; }
+        public string GetName() { return "bot_bozo 3"; }
 
         public void OnMyAIDeath() { }
 
@@ -52,9 +67,18 @@ namespace AI_BehaviorTree_AIImplementation
             List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
             PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
 
+            List<Vector3> playerPos = new List<Vector3>();
+            foreach (PlayerInformations playerInfo in playerInfos)
+            {
+                playerPos.Add(playerInfo.Transform.Position);
+            }
+
             behaviourTree.actions.Clear();
             behaviourTree.gameWorld = AIGameWorldUtils;
+            behaviourTree.myPlayer = myPlayerInfos;
             behaviourTree.start.Execute(ref behaviourTree);
+            behaviourTree.PrevPlayersPos.Clear();
+            behaviourTree.PrevPlayersPos.AddRange(playerPos);
             Debug.Log("count actions : " + behaviourTree.actions.Count);
             return behaviourTree.actions;
         }
